c07f2860e515f67bd229089160877889778438d3
allistermugaisi <allistermugaisi@gmail.com>
2023-05-07 15:05:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1440px',
+			'2xl': { max: '1535px' },
+		},
+		extend: {
+			colors: {
+				darkBlue: '#2B3F69',
+			},
+		},
+	},
+	plugins: [require('daisyui')],
+};
