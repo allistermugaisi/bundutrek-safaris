a3b55152936accd34074e8d4abe0a9ecd474faa9
allistermugaisi <allistermugaisi@gmail.com>
2023-05-10 04:39:02 +0300

    Added content layout to the website

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let slideImg = document.getElementById('slideImg');
+
+const images = new Array(
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683523811/bundutrek-safaris/mara-164_fti9zt.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683544556/bundutrek-safaris/_MG_1557_mqx5jl.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683544556/bundutrek-safaris/_MG_1892_bqsf96.jpg'
+);
+
+const len = images.length;
+
+let i = 0;
+
+function slider() {
+	if (i > len - 1) {
+		i = 0;
+	}
+	slideImg.src = images[i];
+	i++;
+	setTimeout('slider()', 3000);
+}
+
+$(document).ready(function () {
+	var scroll_start = 0;
+	var startchange = $('.nav');
+	var offset = startchange.offset();
+	$(document).scroll(function () {
+		scroll_start = $(this).scrollTop();
+		if (scroll_start > offset.top) {
+			$('.nav').css('background-color', '#FFDCC8');
+		} else {
+			$('.nav').css('background-color', 'transparent');
+		}
+	});
+});
