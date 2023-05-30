89e3fba1814315c6f4a18d486d4d1571303a60dc
allistermugaisi <allistermugaisi@gmail.com>
2023-05-31 01:06:15 +0300

    Refactored suggested packages & removed background on about images

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,11 @@ let slideImg = document.getElementById('slideImg');
 let landcruiserSlideImg = document.getElementById('landcruiserSlideImg');
 
 const images = new Array(
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683523811/bundutrek-safaris/mara-164_fti9zt.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683544556/bundutrek-safaris/_MG_1557_mqx5jl.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1684949105/bundutrek-safaris/WhatsApp_Image_2023-05-22_at_21.05.07_3_tmhjrj.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1684758416/bundutrek-safaris/mara-42_hvldqi.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1684949015/bundutrek-safaris/WhatsApp_Image_2023-05-22_at_21.05.07_nmz39l.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1685483018/bundutrek-safaris/WhatsApp_Image_2023-05-25_at_11.10.28_k6dqz8.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683544556/bundutrek-safaris/_MG_1613_oe9oxv.jpg',
 	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683544556/bundutrek-safaris/_MG_1892_bqsf96.jpg'
 );
 
@@ -31,5 +34,32 @@ $(document).ready(function () {
 		} else {
 			$('.nav').css('background-color', 'transparent');
 		}
+	});
+});
+
+let tabsContainer = document.querySelector('#tabs');
+
+let tabTogglers = tabsContainer.querySelectorAll('#tabs a');
+
+tabTogglers.forEach(function (toggler) {
+	toggler.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		let tabName = this.getAttribute('href');
+
+		let tabContents = document.querySelector('#tab-contents');
+
+		for (let i = 0; i < tabContents.children.length; i++) {
+			tabTogglers[i].parentElement.classList.remove(
+				'border-r-2',
+				'border-red-500'
+			);
+			tabContents.children[i].classList.remove('hidden');
+			if ('#' + tabContents.children[i].id === tabName) {
+				continue;
+			}
+			tabContents.children[i].classList.add('hidden');
+		}
+		e.target.parentElement.classList.add('border-r-2', 'border-red-500');
 	});
 });
