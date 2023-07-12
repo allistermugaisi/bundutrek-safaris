1108c6a44c2adb1d03f4fe21608b98e0e423f33c
allistermugaisi <allistermugaisi@gmail.com>
2023-07-12 13:34:58 +0300

    Changed landing page carousel images

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,11 @@ let slideImg = document.getElementById('slideImg');
 let landcruiserSlideImg = document.getElementById('landcruiserSlideImg');
 
 const images = new Array(
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1686608771/bundutrek-safaris/_MG_2825_pjmleh.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/v1684393890/bundutrek-safaris/IMG_0309_zme15z.jpg',
-	// 'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683525739/bundutrek-safaris/mara-130_ydnvw4.jpg',
-	// 'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683528315/bundutrek-safaris/mara-22_xfrlbx.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683270724/bundutrek-safaris/_MG_4886-2_ay3goq.jpg',
-	// 'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683528315/bundutrek-safaris/mara-26_aox8et.jpg',
-	// 'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683544557/bundutrek-safaris/_MG_1963_gltorp.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1683270722/bundutrek-safaris/39b756_46f36f46f87744688c0a12fa5f97b0f1_mv2_d_4996_3129_s_4_2.jpg_sgmz87.webp',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1684949015/bundutrek-safaris/WhatsApp_Image_2023-05-22_at_21.05.07_nmz39l.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1686428600/bundutrek-safaris/14beff4c-7fdc-4c5c-acd7-d9931e14221b_w6srd8.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1686428973/bundutrek-safaris/7168e364-5ad0-4761-ac56-9069016ad192_uys4jq.jpg',
-	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1686429148/bundutrek-safaris/a995d521-6f6c-46cc-9f0e-72ebeec154bb_gyjxw3.jpg'
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1689157199/bundutrek-safaris/WhatsApp_Image_2023-07-09_at_20.29.48_jlqvi3.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1689157202/bundutrek-safaris/WhatsApp_Image_2023-07-09_at_20.29.46_yvpakr.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1689157204/bundutrek-safaris/WhatsApp_Image_2023-07-09_at_20.29.45_glowjs.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1689157245/bundutrek-safaris/WhatsApp_Image_2023-07-09_at_20.29.38_1_ub3gjx.jpg',
+	'https://res.cloudinary.com/dgisuffs0/image/upload/q_auto/v1689157207/bundutrek-safaris/WhatsApp_Image_2023-07-09_at_20.29.40_ayftgc.jpg'
 );
 
 const len = images.length;
